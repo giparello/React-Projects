acf41ebd380a85b117e2a620e42308fd20486744
giparello <giparello90@gmail.com>
2023-03-14 13:28:21 -0400

    created plain static page

---
+++ first-static-page/src/components/NavBar/index.js
@@ -0,0 +1,9 @@
+import ReactLogo from '../../assets/logo192.png'
+function NavBar(){
+    return(
+        <nav>
+           <img src={ReactLogo} alt="React logo"/>
+        </nav>
+    )
+}
+export default NavBar
--- first-static-page/src/index.js
+++ first-static-page/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MainContent from './components/MainContent';
+import NavBar from './components/NavBar';
 
+//create root. This is technically just a POJO but React makes it easier with JSX
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//render the root
 root.render(
   //THIS IS WHERE YOU SET UP MAIN LAYOUT AREA
   <React.StrictMode>
+    <NavBar/>
+    <MainContent/>
     <App />
   </React.StrictMode>
 );
