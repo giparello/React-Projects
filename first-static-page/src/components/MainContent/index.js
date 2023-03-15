5a43b44bc6f0ea81e055750dfc9f9b0aa3a5a061
giparello <giparello90@gmail.com>
2023-03-15 00:19:40 -0400

    finished reactfact single page site

--- first-static-page/src/components/MainContent/index.js
+++ first-static-page/src/components/MainContent/index.js
@@ -1,18 +1,19 @@
+import './index.scss'
 function MainContent() {
     return (
-        <div>
-            <h1>I'm learning React</h1>
-            <h2>Reasons I love React</h2>
-            <ul>
+        <div className="main-content">
+            <h1 className="header">I'm learning React</h1>
+            <h2 className="header2">Reasons I love React</h2>
+            <ul className="list">
                 <li>It's composable</li>
                 <li>It's declarative</li>
                 <li>It's hireable</li>
                 <li>it is a very popular library</li>
             </ul>
-            <h2>
+            <h2 className="header2">
                 Facts about React
             </h2>
-            <ul>
+            <ul className="list">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100k stars on Github</li>
--- first-static-page/src/components/NavBar/index.js
+++ first-static-page/src/components/NavBar/index.js
@@ -4,13 +4,8 @@ function NavBar(){
     return(
         <nav className='nav'>
            <img src={ReactLogo} alt="React logo" className="nav-logo"/>
-           <ul className="nav-items">
-            <li>
-                Pricing
-            </li>
-            <li>About</li>
-            <li>Contact</li>
-           </ul>
+           <h3>ReactFacts</h3>
+           <h4>React Course-Project 1</h4>
         </nav>
     )
 }
