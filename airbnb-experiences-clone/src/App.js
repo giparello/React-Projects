e07ed2961f6e4d8a3e0fe172f866c7ab5e31be61
giparello <giparello90@gmail.com>
2023-03-26 21:23:06 -0400

    used props to print each card

--- airbnb-experiences-clone/src/App.js
+++ airbnb-experiences-clone/src/App.js
@@ -1,12 +1,38 @@
-import './App.scss';
+import React from "react"
+import Card from "./components/Card"
+import data from "./data"
+import NavBar from "./components/NavBar"
+/*
+Challenge:
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
+
+export default function App() {
+            // <Hero />
+    const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <NavBar/>
+            {cards}
+        </div>
+    )
 }
-
-export default App;
--- airbnb-experiences-clone/src/components/Card/index.js
+++ airbnb-experiences-clone/src/components/Card/index.js
@@ -1,17 +1,17 @@
-import star from '../../assets/star.png'
-function Card(props) {
+import React from "react"
+
+export default function Card(props) {
     return (
         <div className="card">
-            <img src={props.image} className="card--image" alt='card'/>
-            <div className="card-stats">
-            <img src={star} className="star" alt='star' />
-            <span>{props.rating}</span>
-            <span className="gray">({props.reviewnumber})</span>
-            <span className="gray"> {props.location}</span>
+            <img src={`../images/${props.img}`} className="card--image" />
+            <div className="card--stats">
+                <img src="../images/star.png" className="card--star" />
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
-        <p>{props.topic}</p>
-        <p><span className="bold">${props.cost} </span>per person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
-export default Card
