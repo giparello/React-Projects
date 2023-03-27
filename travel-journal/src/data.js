c49815dc526915e10e58a1818890651528ff058b
giparello <giparello90@gmail.com>
2023-03-27 16:51:52 -0400

    created travel journal

---
+++ travel-journal/src/data.js
@@ -0,0 +1,20 @@
+const data =[{
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate:"12 Jan, 2021",
+    endDate:"24 Jan, 2021",
+    description:"Mount Fuji is the tallest mountain in Japan standing at 3,776 meters. Mount Fuji is the single most popular tourist site in Japan for both Japanese and forein tourists",
+    imageUrl:"https://unsplash.com/photos/WLxQvbMyfas"
+},
+{
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate:"12 Jan, 2021",
+    endDate:"24 Jan, 2021",
+    description:"Mount Fuji is the tallest mountain in Japan standing at 3,776 meters. Mount Fuji is the single most popular tourist site in Japan for both Japanese and forein tourists",
+    imageUrl:"https://unsplash.com/photos/WLxQvbMyfas"
+}
+]
+export default data
