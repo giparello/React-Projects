8f3d9c4e8f2ec9bc83cda8f57b6f70e086bde3f8
giparello <giparello90@gmail.com>
2023-03-27 16:36:28 -0400

    simplified card component to recieve entire item

--- airbnb-experiences-clone/src/components/Card/index.js
+++ airbnb-experiences-clone/src/components/Card/index.js
@@ -3,24 +3,24 @@ import star from '../../images/star.png'
 
 export default function Card(props) {
     let badgeText
-    if(props.openSpots ===0){
+    if(props.item.openSpots ===0){
         badgeText= "SOLD OUT"
-    }else if (props.location === "Online"){
+    }else if (props.item.location === "Online"){
         badgeText="ONLINE"
     }
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
 
-            <img src={`/images/${props.img}`} className="card--image" alt="card" />
+            <img src={`/images/${props.item.coverImg}`} className="card--image" alt="card" />
             <div className="card--stats">
                 <img src={star} className="card-star" alt="card" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p>{props.item.title}</p>
+            <p><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
