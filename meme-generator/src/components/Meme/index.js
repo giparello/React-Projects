cff0cd7f3a2273b43e26825751d59762464e5c02
giparello <giparello90@gmail.com>
2023-03-28 17:04:52 -0400

    began adding event listeners

--- meme-generator/src/components/Meme/index.js
+++ meme-generator/src/components/Meme/index.js
@@ -1,12 +1,22 @@
 import './index.scss'
+import memesData from '../../memesData'
 function Meme(){
+
+function getMemeImage(){
+    const memesArray = memesData.data.memes
+    const randomNumber=Math.floor(Math.random()*memesArray.length)
+    const url=memesArray[randomNumber].url
+
+}
+
     return(
         <main>
-            <form className='meme-form'>
+            
+            <div className='meme-form'>
                 <input placeholder='top text' type="text" className='form-input'></input>
                 <input placeholder='bottom text' type="text"className='form-input'></input>
-                <button className='form-button'>Get a new meme image</button>
-            </form>
+                <button onClick={getMemeImage} className='form-button'>Get a new meme image</button>
+            </div>
         </main>
     )
 }
