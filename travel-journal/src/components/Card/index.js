5794ac8e29720268c3e2a98e55f3f6c6ea2180b6
giparello <giparello90@gmail.com>
2023-03-27 20:37:29 -0400

    added see on map button and cleaned up code

--- travel-journal/src/components/Card/index.js
+++ travel-journal/src/components/Card/index.js
@@ -2,10 +2,11 @@ import './index.scss'
 function Card(props) {
     return (
         <div className="card">
-            <h1 className="title">{props.item.title}</h1>
-            <p className="location">{props.item.location}</p>
-            <a className="url" href={props.item.googleMapsUrl}>Location (google maps)</a>
-            <p className="date">{props.item.startDate} - {props.item.endDate} </p>
+            <h1 className="title">{props.item.title}, {props.item.location}</h1>
+            <div className="info">
+            <p className="date"> Visited: {props.item.startDate} - {props.item.endDate} </p>
+            </div>
+            <a className="url" href={props.item.googleMapsUrl}>See on map</a>
             <p className="description">{props.item.description}</p>
             <img className="image" src={props.item.imageUrl} alt="location view" />
         </div>
