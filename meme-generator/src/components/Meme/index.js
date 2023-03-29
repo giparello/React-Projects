3a50184eaa3acd14566ebf740a680f241ec20f6d
giparello <giparello90@gmail.com>
2023-03-29 17:45:03 -0400

    utillized state with object to pass image through

--- meme-generator/src/components/Meme/index.js
+++ meme-generator/src/components/Meme/index.js
@@ -3,15 +3,23 @@ import memesData from '../../memesData'
 import React from 'react'
 function Meme(){
 
+const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText:"",
+    randomImage:"https://i.imgflip.com/tau4.jpg"
+})
 
-const [currentImage, setCurrentImage]= React.useState('')
+const [allMemeImages, setAllMemeImages]=React.useState(memesData)
 
 
 function getMemeImage(){
-    const memesArray = memesData.data.memes
+    const memesArray = allMemeImages.data.memes
     const randomNumber=Math.floor(Math.random()*memesArray.length)
     const url=memesArray[randomNumber].url
-    setCurrentImage(url)
+    setMeme(prevMeme =>({
+        ...prevMeme,
+        randomImage: url
+    }))
 
 }
 
@@ -22,7 +30,7 @@ function getMemeImage(){
                 <input placeholder='top text' type="text" className='form-input'></input>
                 <input placeholder='bottom text' type="text"className='form-input'></input>
                 <button onClick={getMemeImage} className='form-button'>Get a new meme image</button>
-                <img className='meme-image' src={currentImage} alt="meme"/>
+                <img className='meme-image' src={meme.randomImage} alt="meme"/>
             </div>
         </main>
     )
